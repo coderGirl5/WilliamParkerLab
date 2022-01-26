c0d620afb5cad41d45646fae853ecf49d66f6b6f
Wendy Parker <wendy__parker@hotmail.com>
2022-01-25 23:28:19 -0500

    update content

--- angular-app/src/app/core/components/nav.component.ts
+++ angular-app/src/app/core/components/nav.component.ts
@@ -4,15 +4,22 @@ import { Component } from '@angular/core';
   selector: 'app-nav',
   template: `
     <nav class="menu">
+    <p class="menu-label">Welcome</p>
+      <ul class="menu-list">
+        <a routerLink="/about" routerLinkActive="router-link-active">
+          <span>About Us</span>
+        </a>
+      </ul>
       <p class="menu-label">Autism Research</p>
       <ul class="menu-list">
-          <a routerLink="/research" routerLinkActive="router-link-active">
+          <a routerLink="/autism" routerLinkActive="router-link-active">
             <span>The Link Between Autism and Acetaminophen</span>
           </a>
-          <a routerLink="/about" routerLinkActive="router-link-active">
-            <span>About Us</span>
-          </a>
+          <a routerLink="/appendix" routerLinkActive="router-link-active">
+          <span>The Appendix as a Safe House</span>
+        </a>
       </ul>
+   
       <img src="assets/Images/WilliamParkerLabLogo.png" />
     </nav>
   `,
--- angular-app/src/app/router.ts
+++ angular-app/src/app/router.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { AutismComponent } from './autism/autism.component';
+import { AppendixComponent } from './appendix/appendix.component';
 import { NotFoundComponent } from './core';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'research' },
-  { path: 'research', component: AutismComponent},
+  { path: '', pathMatch: 'full', redirectTo: 'autism' },
+  { path: 'appendix', component: AppendixComponent},
+  { path: 'autism', component: AutismComponent},
   { path: 'about', component: AboutComponent },
   { path: '**', component: NotFoundComponent },
 ];
